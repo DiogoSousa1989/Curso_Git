4ff76658b6f9c88fcbba9475de08a5fa3983142f
Diogo Sousa <diogoapsousa@hotmail.com>
2019-12-24 10:19:27 +0000

    Projeto iniciado, mostrando Ola mundo

---
+++ ConsoleApp1/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Calculator
+    {
+        public int num { get; set; }
+
+        public static void calcular(ref int num)
+        {
+           num *= 2;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Número: ");
+            int n = int.Parse(Console.ReadLine());
+            Calculator.calcular(ref n);
+            Console.WriteLine(n);
+        }
+    }
+}
